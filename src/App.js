1087e09c68991c61288bd41ca216c95edcca4028
srinadh1304 <srinadh1304@gmail.com>
2021-11-10 21:59:42 +0530

    ADDED: displaying hello world using react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+        <div>
+          <h1>Hello from Bridgelabz!</h1>
+        </div>
+    );
+  }
+}
+export default App;
