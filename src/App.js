845005a7eedd7cd9569bc16d7fad372ae1beacce
srinadh1304 <srinadh1304@gmail.com>
2021-11-10 23:24:00 +0530

    MODIFIED: displaying user input name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      userName : ''
+      userName : '',
+      nameError : ''
     }
   }
 
@@ -18,7 +19,13 @@ class App extends React.Component {
 
   onNameChange = (event) =>{
     console.log("value is ",event.target.value);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
     this.setState({userName:event.target.value});
+    if(nameRegex.test(event.target.value)){
+      this.setState({nameError:''})
+    }else{
+      this.setState({nameError:'Name is incorrect'})
+    }
   }
 
   render() 
@@ -28,6 +35,11 @@ class App extends React.Component {
         <div>
           <h1>Hello {this.state.userName} from Bridgelabz</h1>
           <img src = {logo} onClick={this.onClick} alt ="The Bridgelabz logo: a Bridge to Employment through lab works"/>
+        </div>
+        <div>
+          <input onChange={this.onNameChange} />
+          <span className="error-output">{this.state.nameError}</span>
+
         </div>
       </>
     
