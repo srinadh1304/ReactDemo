1165678161a0b6cceb2827c318977fbedcac41e5
srinadh1304 <srinadh1304@gmail.com>
2021-11-10 23:10:06 +0530

    ADDED: displaying bridgelabz icon using binding proeprty

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './App.css';
+import logo from './assets/download.jpeg'
 
 class App extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-          title :'Hello from Bridgelabz !!'
-        }
-      }
-  render() {
+  constructor() {
+    super()
+    this.state = {
+      title :'Hello from bridgelabz'
+    }
+  }
+  render() 
+  {
     return (
         <div>
           <h1>{this.state.title}</h1>
+          <img src = {logo} alt ="The Bridgelabz logo: a Bridge to Employment through lab works"/>
         </div>
     );
   }
