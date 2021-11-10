f3c555aa5cde1914f976ad870005bb32c9a88701
srinadh1304 <srinadh1304@gmail.com>
2021-11-10 23:13:30 +0530

    MODIFIED: launching bridgelabz in a new tab

--- src/App.js
+++ src/App.js
@@ -3,18 +3,25 @@ import './App.css';
 import logo from './assets/download.jpeg'
 
 class App extends React.Component {
+  url = 'https://www.bridgelabz.com/'
   constructor() {
     super()
     this.state = {
       title :'Hello from bridgelabz'
     }
   }
+
+  onClick = ($event) => {
+    console.log("save button is clicked!", $event);
+    window.open(this.url,"_blank");
+  }
+
   render() 
   {
     return (
         <div>
           <h1>{this.state.title}</h1>
-          <img src = {logo} alt ="The Bridgelabz logo: a Bridge to Employment through lab works"/>
+          <img src = {logo} onClick={this.onClick} alt ="The Bridgelabz logo: a Bridge to Employment through lab works"/>
         </div>
     );
   }
