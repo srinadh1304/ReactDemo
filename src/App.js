da87374cdb8557b64977ed48d24de97fc1e877e5
srinadh1304 <srinadh1304@gmail.com>
2021-11-10 23:20:09 +0530

    MODIFIED: using event binding to display user input name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      title :'Hello from bridgelabz'
+      userName : ''
     }
   }
 
@@ -16,13 +16,21 @@ class App extends React.Component {
     window.open(this.url,"_blank");
   }
 
+  onNameChange = (event) =>{
+    console.log("value is ",event.target.value);
+    this.setState({userName:event.target.value});
+  }
+
   render() 
   {
     return (
+      <>
         <div>
-          <h1>{this.state.title}</h1>
+          <h1>Hello {this.state.userName} from Bridgelabz</h1>
           <img src = {logo} onClick={this.onClick} alt ="The Bridgelabz logo: a Bridge to Employment through lab works"/>
         </div>
+      </>
+    
     );
   }
 }
